6f85d7dee91389a082ed4434f7e684b0f58c55b2
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-19 16:26:38 -0300

    Repositório para exercícios de teste

---
+++ Testes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testes
+{
+    public class Program
+    {
+        public static int[] Adicionar(int[] arrayInicial, int value) 
+        {
+            int[] arrayTemporario = new int[arrayInicial.Length + 1];
+            for (int i = 0; i < arrayInicial.Length; i++) 
+            {
+                arrayTemporario[i] = arrayInicial[i];
+            }
+            arrayTemporario[arrayInicial.Length] = value;
+            return arrayTemporario;
+        }
+        static void Main(string[] args)
+        {
+            int[] arrayInicial = { 90, 70, 50, 80, 60, 85 };
+            arrayInicial = Adicionar(arrayInicial, 75);
+            for (int i = 0;i < arrayInicial.Length;i++)
+            {
+                Console.Write(arrayInicial[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
